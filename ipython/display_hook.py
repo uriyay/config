0171f6f11c54d3880ac88a6196e91aa273bdd173
uriya <uriyay@gmail.com>
2018-11-04 11:15:58 +0200

    Added ipython display_hook

---
+++ ipython/display_hook.py
@@ -0,0 +1,30 @@
+'''
+Places hook for numbers to be displayed as hex values.
+Installtion:
+    1. Copy the file into ~/.ipython/profile_default/startup directory
+    2. Open ipython :)
+'''
+
+original_display = {}
+
+def display_callback(n, p, cycle):
+    p.text('dec: %d, hex: 0x%08x, bin: %s' % (n, n, bin(n)))
+
+def enable_display_hook():
+    '''To print ints as hex, run hexon_ipython().
+    To revert, run disable_display_hook().
+    '''
+    formatter = get_ipython().display_formatter.formatters['text/plain']
+    original_display[int] = formatter.for_type(int)
+    original_display[long] = formatter.for_type(long)
+    formatter.for_type(int, display_callback)
+    formatter.for_type(long, display_callback)
+
+
+def disable_display_hook():
+    '''See documentation for hexon_ipython().'''
+    formatter = get_ipython().display_formatter.formatters['text/plain']
+    formatter.for_type(int, original_display.get(int))
+    formatter.for_type(long, original_display.get(long))
+
+enable_display_hook()
